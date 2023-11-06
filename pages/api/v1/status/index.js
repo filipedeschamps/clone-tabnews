0917e709aee126a20272270aafb45348f1009490
Filipe Deschamps <filiped@gmail.com>
2023-11-06 17:09:49 +0000

    add `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum");
