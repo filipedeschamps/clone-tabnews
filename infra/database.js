ba2e966f52205c6372a09f0023a740f211b22b5e
Filipe Deschamps <filiped@gmail.com>
2023-12-13 20:03:38 +0000

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -9,9 +9,15 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
