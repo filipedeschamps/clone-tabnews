9300edcf6726794ed3d307166541d9072bb49bad
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-12-23 04:04:32 -0300

    makes database.js client.connect() more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,14 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
