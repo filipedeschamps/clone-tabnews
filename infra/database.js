e799fd946ada4c37e922cd34729862e5e63e1020
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-12-29 17:42:07 -0300

    feat: add ssl flag if NODE_ENV is production

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "production",
   });
 
   try {
