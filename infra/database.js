a8f7a9d4414bdb2c51d5041ad053d9ce63c9371a
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-12-29 18:53:30 -0300

    feat: add more robust ssl configuration

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "production",
+    ssl: getSSLValues(),
   });
 
   try {
@@ -25,3 +25,13 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production";
+}
