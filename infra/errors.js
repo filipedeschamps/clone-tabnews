558038ee50d157a16f1328704f7407b67d1eb3a5
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-01-18 19:43:43 -0300

    refactor: use `next-connect` to abstract errors

--- infra/errors.js
+++ infra/errors.js
@@ -1,3 +1,20 @@
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Método não permitido para esse endpoint.");
+    this.name = "MethodNotAllowedError";
+    this.action = "Verifique se o método HTTP é válido para este endpoint.";
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
 export class InternalServerError extends Error {
   constructor({ cause }) {
     super("Um erro interno não esperado aconteceu.", {
