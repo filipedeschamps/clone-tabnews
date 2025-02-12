82124a8d3982825bddebf1ea7349e3056a0929b3
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-02-12 08:56:53 -0300

    feat: create `migrator` model

---
+++ models/migrator.js
@@ -0,0 +1,48 @@
+import migrationRunner from "node-pg-migrate";
+import path from "node:path";
+import database from "infra/database";
+
+const defaultMigrationOptions = {
+  dir: path.resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+      dryRun: false,
+    });
+    return migratedMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+export default migrator;
