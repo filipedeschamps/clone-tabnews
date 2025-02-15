4f39aa13d47a298634e0e13624084408e7332696
Filipe Deschamps <filiped@gmail.com>
2025-02-15 20:36:46 +0000

    feat: hide `migrator` logs

--- models/migrator.js
+++ models/migrator.js
@@ -6,7 +6,7 @@ const defaultMigrationOptions = {
   dryRun: true,
   dir: resolve("infra", "migrations"),
   direction: "up",
-  verbose: true,
+  log: () => {},
   migrationsTable: "pgmigrations",
 };
 
