5f233d5bcc55958e5a62a363d48e602ff78e3ffe
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-04-14 20:58:01 -0300

    feat: hide `migrator` logs

--- models/migrator.js
+++ models/migrator.js
@@ -5,7 +5,7 @@ import database from "infra/database";
 const defaultMigrationOptions = {
   dir: path.resolve("infra", "migrations"),
   direction: "up",
-  verbose: true,
+  log: () => {},
   migrationsTable: "pgmigrations",
 };
 
