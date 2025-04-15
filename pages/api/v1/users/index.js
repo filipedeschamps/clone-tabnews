8711cd2f08ae36541340964c96ce21b62bd46fdd
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-04-14 22:51:53 -0300

    feat: create `user` model and `/api/v1/users` endpoint

---
+++ pages/api/v1/users/index.js
@@ -0,0 +1,15 @@
+import { createRouter } from "next-connect";
+import controller from "infra/controller.js";
+import user from "models/user.js";
+
+const router = createRouter();
+
+router.post(postHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function postHandler(request, response) {
+  const userInputValues = request.body;
+  const newUser = await user.create(userInputValues);
+  return response.status(201).json(newUser);
+}
