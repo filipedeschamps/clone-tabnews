65140f24bf089bd60885aa095bb76ba96daa99b6
Filipe Deschamps <filiped@gmail.com>
2026-03-18 14:42:07 +0000

    test: clear database and run migrations in `GET /api/v1/status` tests
    Os testes de `GET` do endpoint de status utiliza um usuário privilegiado
    e era um teste que estava passando mesmo sem ter as migrations
    executadas. Isto estava funcionando, pois numa bateria completa de
    `npm test`, algum outro teste já teria feito o trabalho de criar as
    tabelas, sendo que quando o teste era executado de forma isolada,
    naturalmente quebrava por não ter a tabela `user` (necessária para criar
    um usuário privilegiado). Então foi só questão de padronizar o hook
    principal para limpar o banco de dados e rodar as migrations igual ao
    restante dos outros testes.
    
    Isto foi apontado pelo aluno `gleniosp` 🤝

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,6 +3,8 @@ import webserver from "infra/webserver.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
 });
 
 describe("GET /api/v1/status", () => {
