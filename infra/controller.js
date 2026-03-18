02329b19dcf71266e0c3a58753748c67aff040c5
Filipe Deschamps <filiped@gmail.com>
2026-03-18 11:34:45 +0000

    refactor: remove `async` from `setSessionCookie()` and `clearSessionCookie()`
    Estes dois métodos do componente `controller.js` estavam declarados como
    funções assíncronas de forma indevida, pois operam apenas de forma
    síncrona.

--- infra/controller.js
+++ infra/controller.js
@@ -40,7 +40,7 @@ function onErrorHandler(error, request, response) {
   response.status(publicErrorObject.statusCode).json(publicErrorObject);
 }
 
-async function setSessionCookie(sessionToken, response) {
+function setSessionCookie(sessionToken, response) {
   const setCookie = cookie.serialize("session_id", sessionToken, {
     path: "/",
     maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
@@ -51,7 +51,7 @@ async function setSessionCookie(sessionToken, response) {
   response.setHeader("Set-Cookie", setCookie);
 }
 
-async function clearSessionCookie(response) {
+function clearSessionCookie(response) {
   const setCookie = cookie.serialize("session_id", "invalid", {
     path: "/",
     maxAge: -1,
