44bc669c4a328970c96e94cb6e566fd483b05a48
Filipe Deschamps <filiped@gmail.com>
2026-03-18 12:24:00 +0000

    test: assert `email` is updated in database after `PATCH /api/v1/users/[username]`
    Depois de aplicar o filtro de saída que agora faz não devolver mais a
    propriedade `email` do `user`, eu simplesmente removi a expectativa
    deste valor ser retornado na resposta da API de todos os testes. E
    apesar de que isto continua válido, no teste de atualização de email
    ficou faltando a cobertura se a propriedade `email` foi atualizada de
    forma persistende no banco de dados. Então este commit busca o usuário
    no banco e verifica se de fato a informação foi alterada como deveria.
    
    Isto foi apontado pelo aluno `IIsrael` 🤝

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -259,6 +259,10 @@ describe("PATCH /api/v1/users/[username]", () => {
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
 
       expect(responseBody.updated_at > responseBody.created_at).toBe(true);
+
+      const userInDatabase = await user.findOneByUsername(createdUser.username);
+
+      expect(userInDatabase.email).toBe("uniqueEmail2@curso.dev");
     });
 
     test("With new `password`", async () => {
