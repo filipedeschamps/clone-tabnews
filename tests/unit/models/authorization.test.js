354d8baff90a980e5426a44bc129f50aa6bfbf8b
Filipe Deschamps <filiped@gmail.com>
2026-03-18 11:44:47 +0000

    test: fix ISO date format in `authorization` unit tests
    Este commit conserta o formato da data que utilizei em um dos testes
    unitários do model `authorization`. Não teve nenhum impacto, pois o
    objeto de `resource` estava apenas em memória e era usado diretamente
    no filtro que hoje não faz validação do formato das coisas, mas no
    irá fazer.

--- tests/unit/models/authorization.test.js
+++ tests/unit/models/authorization.test.js
@@ -84,8 +84,8 @@ describe("models/authorization.js", () => {
         id: 1,
         username: "resource",
         features: ["read:user"],
-        created_at: "2026-0101T00:00:00.000Z",
-        updated_at: "2026-0101T00:00:00.000Z",
+        created_at: "2026-01-01T00:00:00.000Z",
+        updated_at: "2026-01-01T00:00:00.000Z",
         email: "resource@resource.com",
         password: "resource",
       };
@@ -100,8 +100,8 @@ describe("models/authorization.js", () => {
         id: 1,
         username: "resource",
         features: ["read:user"],
-        created_at: "2026-0101T00:00:00.000Z",
-        updated_at: "2026-0101T00:00:00.000Z",
+        created_at: "2026-01-01T00:00:00.000Z",
+        updated_at: "2026-01-01T00:00:00.000Z",
       });
     });
   });
