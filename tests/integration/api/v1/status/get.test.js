6d09fb09d0677a5f597fe05dbcd60c5631c66a37
Filipe Deschamps <filiped@gmail.com>
2026-03-18 14:55:56 +0000

    test: add default user coverage for `GET /api/v1/status`
    O teste do `GET /api/v1/status` testava o retorno do endpoint com um
    usuário anônimo e com um usuário privilegiado, mas não testava com um
    usuário padrão, onde neste caso, não deveria vir a propriedade `version`
    sobre o banco de dados. Esse teste então adiciona esta cobertura.

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -24,6 +24,30 @@ describe("GET /api/v1/status", () => {
     });
   });
 
+  describe("Default user", () => {
+    test("Retrieving current system status", async () => {
+      const createdUser = await orchestrator.createUser();
+      const activatedUser = await orchestrator.activateUser(createdUser);
+      const sessionObject = await orchestrator.createSession(activatedUser);
+
+      const response = await fetch(`${webserver.origin}/api/v1/status`, {
+        headers: {
+          Cookie: `session_id=${sessionObject.token}`,
+        },
+      });
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      expect(responseBody.dependencies.database).not.toHaveProperty("version");
+    });
+  });
+
   describe("Privileged user", () => {
     test("With `read:status:all`", async () => {
       const privilegedUser = await orchestrator.createUser();
